a99ea86c48454f6ea5acf3c529f09be882e7a3ed
joao linux <joaolinux26@gmail.com>
2026-01-04 13:28:06 -0300

    js proonto

--- ICONS/icons.js
+++ ICONS/icons.js
@@ -1,3 +1,48 @@
 const home = document.getElementById("iconHome")
 const play = document.getElementById("iconPlay")
 const gear = document.getElementById("iconGear")
+const login = document.getElementById("iconLogin")
+const resultado = document.getElementById("resultado")
+
+home.addEventListener('click', () => {
+    resultado.innerHTML = `
+    <div class="welcome-container">
+        <h1 class="welcome-title">Seja bem-vindo</h1>
+        <p class="welcome-text">Esse é o nosso app</p>
+    </div>
+    `
+})
+
+play.addEventListener('click', () => {
+    resultado.innerHTML = `
+    <div class="welcome-container">
+        <h1 class="welcome-title">Sua musica</h1>
+        <p class="welcome-text"><audio id="meuAudio" controls>
+  <source src="suaMusica.mp3" type="audio/mpeg">
+  Seu navegador não suporta o elemento de áudio.
+</audio></p>
+    </div>
+    `
+})
+
+gear.addEventListener('click', () => {
+    resultado.innerHTML = `
+    <div class="welcome-container">
+        <h1 class="welcome-title">Configurações</h1>
+        <p class="welcome-text">Ajuste as opções do sistema aqui</p>
+    </div>
+    `
+})
+
+login.addEventListener('click', () => {
+    resultado.innerHTML = `
+    <div class="welcome-container">
+        <h1 class="welcome-title">Login</h1>
+        <p class="welcome-text">
+            <input type="text" placeholder="Usuário">
+            <input type="password" placeholder="Senha">
+            <button>Entrar</button>
+        </p>
+    </div>
+    `
+})
